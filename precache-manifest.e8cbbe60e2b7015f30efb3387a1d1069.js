e6e8b8d0b7336970d37f8a23a90c28b76c4beac0
Owen <owen.payne42@gmail.com>
2020-01-13 02:27:40 -0500

    Updates

---
+++ precache-manifest.e8cbbe60e2b7015f30efb3387a1d1069.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c31eb4bf2decac0b445a29f6261090bb",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "92777941bdd79ed92a3d",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7f5740aabe66e3118021",
+    "url": "/robofriends/static/css/main.3d6cee30.chunk.css"
+  },
+  {
+    "revision": "92777941bdd79ed92a3d",
+    "url": "/robofriends/static/js/2.bd9327d8.chunk.js"
+  },
+  {
+    "revision": "7fc60fe721fa046663bd312b1d917655",
+    "url": "/robofriends/static/js/2.bd9327d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f5740aabe66e3118021",
+    "url": "/robofriends/static/js/main.6f045323.chunk.js"
+  },
+  {
+    "revision": "0d000ca805ddaefdf0fc",
+    "url": "/robofriends/static/js/runtime-main.8ebeaa0d.js"
+  },
+  {
+    "revision": "d7a551d9f7e2e436f8fca63ad734e7a9",
+    "url": "/robofriends/static/media/conthrax-sb-webfont.d7a551d9.woff2"
+  },
+  {
+    "revision": "fd0d8e5e41ca39079f77022391005e28",
+    "url": "/robofriends/static/media/conthrax-sb-webfont.fd0d8e5e.woff"
+  }
+]);
